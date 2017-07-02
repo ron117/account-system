af2c1a56f6d92a785badf625c0c90ad913847a7e
santhosh <santoshmariyala2013@gmail.com>
2017-07-02 21:01:42 +0530

    This is commit on new feature 112

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+# this code is wriiten by ajay and santhosh
   protect_from_forgery with: :exception
     before_action :authenticate_user!
 end
